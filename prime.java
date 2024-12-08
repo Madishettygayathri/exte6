afabfb436bb8a5bf7b153c4aba8039f84f50171c
Madishettygayathri <madishettygayathri94@gmail.com>
2024-12-08 17:47:06 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,18 +1,22 @@
-class prime{
-    public static void main(String[]args)
-    {
-        int n=23,flag=0;
-        for(int i=2;i<=Math.sqrt(n);i++)
-        {
-            if(n%i==0)
-            {
-                flag=1;
-                break;
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100 are:");
+
+        for (int num = 2; num <= 100; num++) { // Loop through numbers 2 to 100
+            boolean isPrime = true;
+
+            // Check if num is divisible by any number from 2 to its square root
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break; // Exit loop if not prime
+                }
+            }
+
+            // If the number is prime, print it
+            if (isPrime) {
+                System.out.print(num + " ");
             }
         }
-        if(flag==0)
-        System.out.println("yes");
-        else
-         System.out.println("no");
     }
 }
